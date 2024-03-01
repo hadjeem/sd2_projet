c4a8bf7b60d09722205f9b48f63372c60a526406
hadje <hadjera.emroska@student.vinci.be>
2024-03-01 14:38:30 +0100

    add: class Graph

---
+++ src/Graph.java
@@ -0,0 +1,18 @@
+import java.io.File;
+
+public class Graph {
+
+public Graph(File cities, File roads) {
+
+  }
+
+  public void calculerItineraireMinimisantNombreRoutes(String depart, String arrivee) {
+
+  }
+
+  public void calculerItineraireMinimisantKm(String depart, String arrivee) {
+
+  }
+
+
+}
